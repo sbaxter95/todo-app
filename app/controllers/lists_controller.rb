0e120f2ac0300af229417dce7e0fa2be9732f554
sbaxter95 <sbaxter95@outlook.com>
2017-10-05 16:52:14 +0100

    index route complete

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,50 @@
+class ListsController < ApplicationController
+    def index
+      @user = User.find(current_user.id)
+      @userid = @user.id
+      @lists = List.all
+      @userlists = @lists.where(user_id: current_user.id)
+    end
+
+    def show
+      @list = List.find(params[:id])
+    end
+
+    def new
+      @list = List.new
+    end
+
+    def create
+      @user = User.find(current_user.id)
+
+      new_item = List.create(list_params)
+      new_item.user_id = current_user.id
+      new_item.save
+
+      redirect_to new_item
+
+    end
+
+    def edit
+      @list = List.find(params[:id])
+    end
+
+    def update
+
+      list = List.find(params[:id])
+      list.update(list_params)
+
+      redirect_to list
+    end
+
+    def destroy
+      List.destroy(params[:id])
+      redirect_to list_url
+    end
+
+    protected
+    def list_params
+      params.require(:list).permit(:item, :user_id)
+    end
+  end
+
